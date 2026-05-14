cf6d3b03d61e153be2d73e42d075b94fad5b28a7
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-05-14 12:45:21 +0600

    useState setCount increment Bug concept added

--- react-tasks/react-class-2/src/App.jsx
+++ react-tasks/react-class-2/src/App.jsx
@@ -8,11 +8,26 @@ import ButtonWithDestructuring from './components/ButtonWithDestructuring';
 
 function App() {
   // Count with button with plain JS : JS runs fine, but it won't trigger re-render of the count
-      let count = 0;
-      function up(){
-        count++;
-        console.log(count);
+      // let count = 0;
+      // function up(){
+      //   count++;
+      //   console.log(count);
         
+      // }
+
+      let [count, setCount] = useState(0);
+      function handleCount(){
+        // setCount(count++);
+        // setCount(count + 1);
+        // console.log("before", count);
+        // setCount((previousValue)=> previousValue + 1);
+        // console.log("after", count);
+        setCount((previousValue)=> {
+          console.log("before", count);
+          console.log("before", previousValue);
+        return previousValue + 1;
+        // console.log("after", count);
+        });
       }
   return (
     <>
@@ -34,6 +49,10 @@ function App() {
       Count with button with plain JS : JS runs fine, but it won't trigger re-renders*/}
           {/* <h1>Count {count} </h1>
         <button onClick={up}>Count Button</button> */}
+
+        {/* using useState */}
+        <h1>Count {count} </h1>
+        <button onClick={handleCount}>Count Button</button>
     </>
   )
 }
