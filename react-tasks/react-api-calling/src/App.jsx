342d25b64e3a32146f372cb3958341ffcda6a90f
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-05-19 12:52:17 +0600

    calling api in react, ? operators, conditional rendering

--- react-tasks/react-api-calling/src/App.jsx
+++ react-tasks/react-api-calling/src/App.jsx
@@ -1,8 +1,29 @@
-function App() {
+import { useEffect, useState } from "react";
 
+function App() {
+// render and re-render jokes data in UI - useState()
+  const [jokesData, setJokesData] = useState(null);
+//  calling API function
+  async function callApi() {
+    const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+    const data = await response.json();
+// setting the fetched and parsed data object as arguement of setJokesData() function
+// as state value to render/re-render, 
+    setJokesData(data); //
+  }
+
+// useEffect to handle side effect like API calling, to fire api call only once initial render
+/*   - only run ONCE after first mount, never again
+     - [] = "don't re-run on re-renders"
+ */
+useEffect(()=>{
+  callApi();
+}, [])
   return (
     <>
-   
+      <h1> Jokes:</h1>
+      <h2> setup: {jokesData?.setup} </h2> {/* ?. (optional chaining operator) for conditional rendering */}
+      <h2>punchline : {jokesData?.punchline} </h2>
     </>
   )
 }
