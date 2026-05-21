f30b2d3be162ae69a8c3c770cbc57d0f4335ec02
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-05-21 13:04:11 +0600

    conditional rendering using try catch w/o ?. operator & loader, theory of try catch, error handle  in react

--- react-tasks/react-api-calling/src/App.jsx
+++ react-tasks/react-api-calling/src/App.jsx
@@ -1,29 +1,59 @@
 import { useEffect, useState } from "react";
 
 function App() {
-// render and re-render jokes data in UI - useState()
-  const [jokesData, setJokesData] = useState(null);
-//  calling API function
-  async function callApi() {
-    const response = await fetch('https://official-joke-api.appspot.com/random_joke');
-    const data = await response.json();
-// setting the fetched and parsed data object as arguement of setJokesData() function
-// as state value to render/re-render, 
-    setJokesData(data); //
-  }
 
+  // render and re-render jokes data in UI - useState()
+  const [jokesData, setJokesData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+//  calling API function  
+  async function callApi() {
+  // try catch to handle error- see more at theory-react
+    try {
+      const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+
+  // catch Http error
+      if (!response.ok) {
+        throw new Error(`HTTP error! Error status: ${response.status} ${response.statusText}`);
+      }
+  // data json
+      const data = await response.json();
+      // setting the fetched and parsed data object as arguement of setJokesData() function, as state value to render/re-render, 
+      setJokesData(data); //
+  
+    } catch (error) {
+      setError(error.message);
+    }
+    finally {
+      setLoading(false); // initially true, so that loader shows if takes time to render 
+    }
+  };
 // useEffect to handle side effect like API calling, to fire api call only once initial render
-/*   - only run ONCE after first mount, never again
-     - [] = "don't re-run on re-renders"
- */
-useEffect(()=>{
-  callApi();
-}, [])
+  /*   - only run ONCE after first mount, never again
+       - [] = "don't re-run on re-renders"
+   */
+  useEffect(() => {
+    callApi();
+  }, [])
+
+// loader & error o/p in UI if exists. works bcz inside function App
+  if (loading) {
+    return <p>Loading.... Looooading...!!</p>
+  }
+  if (error) {
+    return <p>{error}</p>
+  }
   return (
     <>
       <h1> Jokes:</h1>
-      <h2> setup: {jokesData?.setup} </h2> {/* ?. (optional chaining operator) for conditional rendering */}
-      <h2>punchline : {jokesData?.punchline} </h2>
+      {/* before conditional rendering and try catch application (with ?. (optional chaining operator))  ----------------o/p */}
+      {/*<h2> setup: {jokesData?.setup} </h2> */} {/* ?. (optional chaining operator) for conditional rendering */}
+      {/*  <h2>punchline : {jokesData?.punchline} </h2> */}
+
+      {/* after conditional rendering and try catch application (without ?. (optional chaining operator)) ----------------o/p */}
+      <h3> setup: {jokesData.setup} </h3>
+      <h3>punchline : {jokesData.punchline} </h3>
     </>
   )
 }
